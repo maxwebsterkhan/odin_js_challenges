44c325b6b584c1858e52839a7e5f6397a7ca5d75
Max <maxwebsterr@live.co.uk>
2019-11-07 15:08:00 +0000

    challenge complete

--- repeatString/repeatString.js
+++ repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-repeatString ('hey', 3)
-console.log(repeatString)
+const repeatString = function (hello, num) {
+    if (num < 0) return "ERROR"
+    let str = "";
+    for (let i = 0; i < num; i++) {
+        str += hello;
+    }
+    return str;
 }
-
+console.log(str)
 module.exports = repeatString
